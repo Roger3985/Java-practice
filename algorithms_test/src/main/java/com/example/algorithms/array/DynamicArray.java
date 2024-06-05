b4b1aae2c9072dc117e504d7fe7596747ac28c65
a39849686 <a0967067696@gmail.com>
2024-06-06 02:18:04 +0800

    first step by dynamicarray

---
+++ algorithms_test/src/main/java/com/example/algorithms/array/DynamicArray.java
@@ -0,0 +1,12 @@
+package com.example.algorithms.array;
+
+/**
+ * 動態陣列實現
+ */
+public class DynamicArray {
+    private int size = 0; // 邏輯大小
+    private int capacity = 8; // 容量
+    private int[] array = new int[capacity];
+
+
+}
