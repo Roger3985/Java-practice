e437625d3badb81deb0f7306fd35411d071e558f
a39849686 <a0967067696@gmail.com>
2024-06-10 12:22:32 +0800

    dynamicarray

--- algorithms_test/src/main/java/com/example/algorithms/array/DynamicArray.java
+++ algorithms_test/src/main/java/com/example/algorithms/array/DynamicArray.java
@@ -8,5 +8,29 @@ public class DynamicArray {
     private int capacity = 8; // 容量
     private int[] array = new int[capacity];
 
+    /**
+     * 往最後面 [size]加上元素
+     * element - 待添加的元素
+     */
+    public void addLast(int element) {
+        // array[size] = element;
+        // size++;
+        add(size, element); // add 合併 addLast
+    }
 
+    /**
+     * 往後面加上元素
+     */
+    public void add(int index, int element) {
+        if (index < 0 || index > size) {
+            System.arraycopy(array, index,
+                    array, index + 1, size - index);
+        }
+        array[index] = element;
+        size++;
+    }
+
+    public int get(int index) { // [0...size)
+        return array[index];
+    }
 }
---
+++ algorithms_test/src/test/java/com/example/algorithms/TestDynamicArray.java
@@ -0,0 +1,21 @@
+package com.example.algorithms;
+
+import com.example.algorithms.array.DynamicArray;
+import org.junit.jupiter.api.Test;
+
+public class TestDynamicArray {
+
+    @Test
+    public void test1() {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+        dynamicArray.addLast(5);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(dynamicArray.get(i));
+        }
+    }
+}
