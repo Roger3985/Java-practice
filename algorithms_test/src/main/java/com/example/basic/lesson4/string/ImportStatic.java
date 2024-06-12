a34dfbc827d4391e74c098a3fb5db9c38e4fccc1
a39849686 <a0967067696@gmail.com>
2024-06-12 23:08:50 +0800

    java.lang.System.in and System.out using.

---
+++ algorithms_test/src/main/java/com/example/basic/lesson4/string/ImportStatic.java
@@ -0,0 +1,14 @@
+package com.example.basic.lesson4.string;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class ImportStatic {
+    public static void main(String[] args) {
+        var console = new Scanner(in);
+        out.println("Please enter name :");
+        out.printf("%s Hello! %n", console.nextLine());
+    }
+}
