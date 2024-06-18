3f0259e2c96a7f5509da21e8e8a974132197ddfd
a39849686 <a0967067696@gmail.com>
2024-06-19 01:24:58 +0800

    equals and hasCode.

--- algorithms_test/src/main/java/com/example/basic/lesson9/example/Students.java
+++ algorithms_test/src/main/java/com/example/basic/lesson9/example/Students.java
@@ -23,13 +23,47 @@ class Student {
     public String toString() {
         return "(%s, %s)".formatted(name, number);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (number == null) {
+            if (other.number != null) {
+                return false;
+            }
+        } else if (!number.equals(other.number)) {
+            return false;
+        }
+        return true;
+    }
 }
 public class Students {
     public static void main(String[] args) {
         var students = new HashSet();
         students.add(new Student("John", "123"));
         students.add(new Student("Jane", "456"));
-        students.add(new Student("Jack", "789"));
+        students.add(new Student("John", "123"));
         System.out.println(students);
     }
 }
