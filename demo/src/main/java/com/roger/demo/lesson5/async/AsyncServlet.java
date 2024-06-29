600eb371fec66dc275082199026450c71a7e1b25
a39849686 <a0967067696@gmail.com>
2024-06-30 03:17:09 +0800

    jsp

--- demo/src/main/java/com/roger/demo/lesson5/async/AsyncServlet.java
+++ demo/src/main/java/com/roger/demo/lesson5/async/AsyncServlet.java
@@ -1,46 +1,46 @@
-package com.roger.demo.lesson5.async;
-
-import jakarta.servlet.AsyncContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.concurrent.CompletableFuture;
-
-@WebServlet(
-        urlPatterns = {"/async"},
-        asyncSupported = true // 標記 Servlet 支援非同步處理
-)
-public class AsyncServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=utf-8");
-        AsyncContext ctx = request.startAsync(); // 開始非同步處理，釋放執行緒
-
-        // 建立非同步處理任務的 CompletableFuture
-        asyncResource(ctx).thenAcceptAsync(resource -> {
-            try {
-                ctx.getResponse().getWriter().println(resource);
-                ctx.complete(); // 對瀏覽器完成回應
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        });
-    }
-
-    private CompletableFuture<String> asyncResource(AsyncContext ctx) {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                String resource = ctx.getRequest().getParameter("resource");
-                Thread.sleep(10000);
-                return String.format("%s back finally...XD",
-                        resource.toUpperCase());
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-}
+//package com.roger.demo.lesson5.async;
+//
+//import jakarta.servlet.AsyncContext;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//import java.io.UncheckedIOException;
+//import java.util.concurrent.CompletableFuture;
+//
+//@WebServlet(
+//        urlPatterns = {"/async"},
+//        asyncSupported = true // 標記 Servlet 支援非同步處理
+//)
+//public class AsyncServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.setContentType("text/html;charset=utf-8");
+//        AsyncContext ctx = request.startAsync(); // 開始非同步處理，釋放執行緒
+//
+//        // 建立非同步處理任務的 CompletableFuture
+//        asyncResource(ctx).thenAcceptAsync(resource -> {
+//            try {
+//                ctx.getResponse().getWriter().println(resource);
+//                ctx.complete(); // 對瀏覽器完成回應
+//            } catch (IOException e) {
+//                throw new UncheckedIOException(e);
+//            }
+//        });
+//    }
+//
+//    private CompletableFuture<String> asyncResource(AsyncContext ctx) {
+//        return CompletableFuture.supplyAsync(() -> {
+//            try {
+//                String resource = ctx.getRequest().getParameter("resource");
+//                Thread.sleep(10000);
+//                return String.format("%s back finally...XD",
+//                        resource.toUpperCase());
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+//    }
+//}
--- demo/src/main/java/com/roger/demo/lesson5/async/AsyncUpload.java
+++ demo/src/main/java/com/roger/demo/lesson5/async/AsyncUpload.java
@@ -1,122 +1,122 @@
-package com.roger.demo.lesson5.async;
-
-import jakarta.servlet.AsyncContext;
-import jakarta.servlet.ReadListener;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletInputStream;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-@WebServlet(
-        urlPatterns = { "/asyncUpload" },
-        asyncSupported = true
-)
-public class AsyncUpload extends HttpServlet {
-    private final Pattern fileNameRegex =
-            Pattern.compile("filename=\"(.*)\"");
-
-    private final Pattern fileRangeRegex =
-            Pattern.compile("filename=\".*\"\\r\\n.*\\r\\n\\r\\n(.*+)");
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        AsyncContext ctx = request.startAsync();
-
-        ServletInputStream in = request.getInputStream();
-
-        in.setReadListener(new ReadListener() {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-            @Override
-            public void onDataAvailable() throws IOException {
-                byte[] buffer = new byte[1024];
-                int length = -1;
-                while(in.isReady() && (length = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, length);
-                }
-            }
-
-            @Override
-            public void onAllDataRead() throws IOException {
-                byte[] content = out.toByteArray();
-                String contentAsTxt = new String(content, "ISO-8859-1");
-
-                String filename = filename(contentAsTxt);
-                Range fileRange = fileRange(contentAsTxt, request.getContentType());
-                write(content,
-                        contentAsTxt.substring(0, fileRange.start)
-                                .getBytes("ISO-8859-1")
-                                .length,
-                        contentAsTxt.substring(0, fileRange.end)
-                                .getBytes("ISO-8859-1")
-                                .length,
-                        String.format("c:/workspace/%s", filename)
-                );
-
-                response.getWriter().println("Upload Successfully");
-                ctx.complete();
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                ctx.complete();
-                throw new RuntimeException(throwable);
-            }
-        });
-    }
-
-    // 取得檔案名稱
-    private String filename(String contentTxt)
-            throws UnsupportedEncodingException {
-        Matcher matcher = fileNameRegex.matcher(contentTxt);
-        matcher.find();
-
-        String filename =  matcher.group(1);
-        // 如果名稱上包含資料夾符號「\」，就只取得最後的檔名
-        if(filename.contains("\\")) {
-            return filename.substring(filename.lastIndexOf("\\") + 1);
-        }
-        return filename;
-    }
-
-    // 封裝範圍起始與結束
-    private static class Range {
-        final int start;
-        final int end;
-        public Range(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-
-    // 取得檔案邊界範圍
-    private Range fileRange(String content, String contentType) {
-        Matcher matcher = fileRangeRegex.matcher(content);
-        matcher.find();
-        int start = matcher.start(1);
-
-        String boundary = contentType.substring(
-                contentType.lastIndexOf("=") + 1, contentType.length());
-        int end = content.indexOf(boundary, start) - 4;
-
-        return new Range(start, end);
-    }
-
-    // 儲存檔案內容
-    private void write(byte[] content, int start, int end, String file)
-            throws IOException {
-        try(FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-            fileOutputStream.write(content, start, (end - start));
-        }
-    }
-}
+//package com.roger.demo.lesson5.async;
+//
+//import jakarta.servlet.AsyncContext;
+//import jakarta.servlet.ReadListener;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.ServletInputStream;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.UnsupportedEncodingException;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//@WebServlet(
+//        urlPatterns = { "/asyncUpload" },
+//        asyncSupported = true
+//)
+//public class AsyncUpload extends HttpServlet {
+//    private final Pattern fileNameRegex =
+//            Pattern.compile("filename=\"(.*)\"");
+//
+//    private final Pattern fileRangeRegex =
+//            Pattern.compile("filename=\".*\"\\r\\n.*\\r\\n\\r\\n(.*+)");
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        AsyncContext ctx = request.startAsync();
+//
+//        ServletInputStream in = request.getInputStream();
+//
+//        in.setReadListener(new ReadListener() {
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//            @Override
+//            public void onDataAvailable() throws IOException {
+//                byte[] buffer = new byte[1024];
+//                int length = -1;
+//                while(in.isReady() && (length = in.read(buffer)) != -1) {
+//                    out.write(buffer, 0, length);
+//                }
+//            }
+//
+//            @Override
+//            public void onAllDataRead() throws IOException {
+//                byte[] content = out.toByteArray();
+//                String contentAsTxt = new String(content, "ISO-8859-1");
+//
+//                String filename = filename(contentAsTxt);
+//                Range fileRange = fileRange(contentAsTxt, request.getContentType());
+//                write(content,
+//                        contentAsTxt.substring(0, fileRange.start)
+//                                .getBytes("ISO-8859-1")
+//                                .length,
+//                        contentAsTxt.substring(0, fileRange.end)
+//                                .getBytes("ISO-8859-1")
+//                                .length,
+//                        String.format("c:/workspace/%s", filename)
+//                );
+//
+//                response.getWriter().println("Upload Successfully");
+//                ctx.complete();
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//                ctx.complete();
+//                throw new RuntimeException(throwable);
+//            }
+//        });
+//    }
+//
+//    // 取得檔案名稱
+//    private String filename(String contentTxt)
+//            throws UnsupportedEncodingException {
+//        Matcher matcher = fileNameRegex.matcher(contentTxt);
+//        matcher.find();
+//
+//        String filename =  matcher.group(1);
+//        // 如果名稱上包含資料夾符號「\」，就只取得最後的檔名
+//        if(filename.contains("\\")) {
+//            return filename.substring(filename.lastIndexOf("\\") + 1);
+//        }
+//        return filename;
+//    }
+//
+//    // 封裝範圍起始與結束
+//    private static class Range {
+//        final int start;
+//        final int end;
+//        public Range(int start, int end) {
+//            this.start = start;
+//            this.end = end;
+//        }
+//    }
+//
+//    // 取得檔案邊界範圍
+//    private Range fileRange(String content, String contentType) {
+//        Matcher matcher = fileRangeRegex.matcher(content);
+//        matcher.find();
+//        int start = matcher.start(1);
+//
+//        String boundary = contentType.substring(
+//                contentType.lastIndexOf("=") + 1, contentType.length());
+//        int end = content.indexOf(boundary, start) - 4;
+//
+//        return new Range(start, end);
+//    }
+//
+//    // 儲存檔案內容
+//    private void write(byte[] content, int start, int end, String file)
+//            throws IOException {
+//        try(FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+//            fileOutputStream.write(content, start, (end - start));
+//        }
+//    }
+//}
