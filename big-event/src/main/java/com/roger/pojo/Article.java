a05d4ef7054894a9e23533b3a36b2d9a614138e9
a39849686 <a0967067696@gmail.com>
2024-07-02 02:31:57 +0800

    pojo

--- big-event/src/main/java/com/roger/pojo/Article.java
+++ big-event/src/main/java/com/roger/pojo/Article.java
@@ -1,7 +1,10 @@
 package com.roger.pojo;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class Article {
     private int id;
     private String title;
@@ -13,91 +16,4 @@ public class Article {
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
 
-    // Constructors
-    public Article() {}
-
-    public Article(int id, String title, String content, String coverImg, String state, int categoryId, int createUser, LocalDateTime createTime, LocalDateTime updateTime) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.coverImg = coverImg;
-        this.state = state;
-        this.categoryId = categoryId;
-        this.createUser = createUser;
-        this.createTime = createTime;
-        this.updateTime = updateTime;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getCoverImg() {
-        return coverImg;
-    }
-
-    public void setCoverImg(String coverImg) {
-        this.coverImg = coverImg;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public int getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(int createUser) {
-        this.createUser = createUser;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
 }
--- big-event/src/main/java/com/roger/pojo/Category.java
+++ big-event/src/main/java/com/roger/pojo/Category.java
@@ -1,7 +1,10 @@
 package com.roger.pojo;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class Category {
     private int id;
     private String categoryName;
@@ -9,65 +12,4 @@ public class Category {
     private int createUser;
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
-
-    // Constructors
-    public Category() {}
-
-    public Category(int id, String categoryName, String categoryAlias, int createUser, LocalDateTime createTime, LocalDateTime updateTime) {
-        this.id = id;
-        this.categoryName = categoryName;
-        this.categoryAlias = categoryAlias;
-        this.createUser = createUser;
-        this.createTime = createTime;
-        this.updateTime = updateTime;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public String getCategoryAlias() {
-        return categoryAlias;
-    }
-
-    public void setCategoryAlias(String categoryAlias) {
-        this.categoryAlias = categoryAlias;
-    }
-
-    public int getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(int createUser) {
-        this.createUser = createUser;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
 }
