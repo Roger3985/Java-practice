49a6aef764ee41d929dec43fee183dfb9588a332
a39849686 <a0967067696@gmail.com>
2024-07-02 22:45:54 +0800

    spring validation

--- big-event/src/main/java/com/roger/controller/UserController.java
+++ big-event/src/main/java/com/roger/controller/UserController.java
@@ -3,11 +3,14 @@ package com.roger.controller;
 import com.roger.pojo.Result;
 import com.roger.pojo.User;
 import com.roger.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -16,7 +19,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
+    public Result register(@Pattern(regexp = "^\\${5,16}") String username, @Pattern(regexp = "^\\${5,16}") String password) {
+
         // 查詢會員
         User u = userService.findByUserName(username);
         if (u == null) {
